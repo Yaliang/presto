0af8df6a36328881d942a8d163a26fc380d238eb
Yaliang Wang <yaliangw@twitter.com>
2018-01-05 20:10:08 -0800

    test

--- presto-product-tests/bin/run_on_docker.sh
+++ presto-product-tests/bin/run_on_docker.sh
@@ -46,7 +46,7 @@ function run_in_application_runner_container() {
 
 function check_presto() {
   if [[ -v PRESTO_PASSWORD ]]; then
-    run_in_application_runner_container export PRESTO_PASSWORD=${PRESTO_PASSWORD}
+    docker exec $(application-runner) export PRESTO_PASSWORD=${PRESTO_PASSWORD}
   fi
   run_in_application_runner_container \
     java -jar "/docker/volumes/presto-cli/presto-cli-executable.jar" \
