c61dc07190d6b684d303679c655e244a0433bb69
Yaliang Wang <yaliangw@twitter.com>
2018-01-05 18:50:46 -0800

    test

--- presto-product-tests/bin/run_on_docker.sh
+++ presto-product-tests/bin/run_on_docker.sh
@@ -192,6 +192,9 @@ CLI_ARGUMENTS="--server presto-master:8080"
 if [[ "$ENVIRONMENT" == "multinode-tls" ]]; then
     CLI_ARGUMENTS="--server https://presto-master.docker.cluster:7778 --keystore-path /docker/volumes/conf/presto/etc/docker.cluster.jks --keystore-password 123456"
 fi
+if [[ "$ENVIRONMENT" == "singlenode-ldap" ]]; then
+    CLI_ARGUMENTS="--server https://presto-master:8443 --keystore-path /etc/openldap/certs/cacerts.jks --keystore-password testldap --user DefaultGroupUser --password LDAPPass123"
+fi
 
 # check docker and docker compose installation
 docker-compose version
